5fe1d07faf1c86d1d00b3fb2a51d1e05ed1cc5e2
Rahman <rahmanhasibu2@gmail.com>
2026-07-14 14:24:41 +0530

    Completed Hands On 3: Hello World RESTful Web Service

--- Week-3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week-3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,24 +1,13 @@
+
 package com.cognizant.springlearn;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringLearnApplication {
 
     public static void main(String[] args) {
-
-        displayCountry();
-
-    }
-
-    public static void displayCountry() {
-
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
-
-        System.out.println("Country : " + country);
-
+        SpringApplication.run(SpringLearnApplication.class, args);
     }
 }
